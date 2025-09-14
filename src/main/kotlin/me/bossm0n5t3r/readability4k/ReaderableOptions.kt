9c18b7c388a1dc325dbf9edf81979d740afe2dfb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-09-14 23:30:00 +0900

    feat: add ReaderableOptions data class
    - Define configuration options for the `isProbablyReaderable` function
    - Provide default values for `minContentLength`, `minScore`, and `visibilityChecker`

---
+++ src/main/kotlin/me/bossm0n5t3r/readability4k/ReaderableOptions.kt
@@ -0,0 +1,16 @@
+package me.bossm0n5t3r.readability4k
+
+import org.jsoup.nodes.Element
+
+/**
+ * Configuration options for isProbablyReaderable function.
+ *
+ * @param minContentLength The minimum node content length used to decide if the document is readerable.
+ * @param minScore The minimum cumulated 'score' used to determine if the document is readerable.
+ * @param visibilityChecker The function used to determine if a node is visible.
+ */
+data class ReaderableOptions(
+    val minContentLength: Int = 140,
+    val minScore: Int = 20,
+    val visibilityChecker: (Element) -> Boolean = ::isNodeVisible,
+)
