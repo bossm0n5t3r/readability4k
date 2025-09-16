4998cccd20907be8a6fc7cdddada65ee057b888c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-09-16 21:40:00 +0900

    refactor: update TestPage expectedMetadata type
    - Change `expectedMetadata` value type from `Map<String, Any?>` to `Map<String, JsonElement>`

--- src/test/kotlin/me/bossm0n5t3r/readability4k/TestPage.kt
+++ src/test/kotlin/me/bossm0n5t3r/readability4k/TestPage.kt
@@ -1,8 +1,10 @@
 package me.bossm0n5t3r.readability4k
 
+import kotlinx.serialization.json.JsonElement
+
 data class TestPage(
     val dir: String,
     val source: String,
     val expectedContent: String,
-    val expectedMetadata: Map<String, Any?>,
+    val expectedMetadata: Map<String, JsonElement>,
 )
