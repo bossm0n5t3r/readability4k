963c5546add3479ae4fbb7d37e0492e8b80ff66f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-09-16 22:00:00 +0900

    feat: add utility functions for test page handling
    - Implement `Utils` object with file reading, JSON parsing, and test page handling utilities
    - Add HTML pretty-printing function using Jsoup

---
+++ src/test/kotlin/me/bossm0n5t3r/readability4k/Utils.kt
@@ -0,0 +1,48 @@
+package me.bossm0n5t3r.readability4k
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
+import org.jsoup.Jsoup
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.div
+import kotlin.io.path.isDirectory
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.name
+import kotlin.io.path.readText
+
+object Utils {
+    private fun readFile(filePath: Path): String = filePath.readText().trim()
+
+    private fun readJSON(jsonPath: Path): Map<String, JsonElement> {
+        val jsonContent = readFile(jsonPath)
+        return Json.parseToJsonElement(jsonContent).jsonObject.toMap()
+    }
+
+    private val testPageRoot = Path("src/test/resources/test-pages")
+
+    fun getTestPages(): List<TestPage> =
+        testPageRoot
+            .listDirectoryEntries()
+            .filter { it.isDirectory() }
+            .map { dir ->
+                TestPage(
+                    dir = dir.name,
+                    source = readFile(dir / "source.html"),
+                    expectedContent = readFile(dir / "expected.html"),
+                    expectedMetadata = readJSON(dir / "expected-metadata.json"),
+                )
+            }
+
+    fun prettyPrint(html: String): String =
+        Jsoup
+            .parseBodyFragment(html)
+            .apply {
+                outputSettings().apply {
+                    indentAmount(4)
+                    prettyPrint(true)
+                }
+            }.body()
+            .html()
+}
