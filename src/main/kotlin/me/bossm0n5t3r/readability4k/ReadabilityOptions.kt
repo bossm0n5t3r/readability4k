ec6e5ee1dc3a7b26cc13b0922b1067f6c4d5a86f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-09-21 21:10:00 +0900

    feat: add `ReadabilityOptions` data class
    - Define options for configuring readability parsing

---
+++ src/main/kotlin/me/bossm0n5t3r/readability4k/ReadabilityOptions.kt
@@ -0,0 +1,17 @@
+package me.bossm0n5t3r.readability4k
+
+import org.jsoup.nodes.Element
+import java.math.BigDecimal
+
+data class ReadabilityOptions(
+    val debug: Boolean = false,
+    val maxElemsToParse: Int = DEFAULT_MAX_ELEMS_TO_PARSE,
+    val nbTopCandidates: Int = DEFAULT_N_TOP_CANDIDATES,
+    val charThreshold: Int = DEFAULT_CHAR_THRESHOLD,
+    val classesToPreserve: List<String> = emptyList(),
+    val keepClasses: Boolean = false,
+    val serializer: ((Element) -> String) = { element -> element.html() },
+    val disableJSONLD: Boolean = false,
+    val allowedVideoRegex: Regex = Regexps.VIDEOS,
+    val linkDensityModifier: BigDecimal = BigDecimal.ONE,
+)
