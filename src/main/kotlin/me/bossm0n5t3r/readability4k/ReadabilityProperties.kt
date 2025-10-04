4f4b1562245ad7025271739efbf783d401b74a37
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-10-04 17:45:00 +0900

    feat: add `ReadabilityProperties` data class
    - Define properties for configuring readability extraction
    - Introduce inner `Attempt` data class for parsing attempts

---
+++ src/main/kotlin/me/bossm0n5t3r/readability4k/ReadabilityProperties.kt
@@ -0,0 +1,32 @@
+package me.bossm0n5t3r.readability4k
+
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.math.BigDecimal
+
+data class ReadabilityProperties(
+    val document: Document,
+    val articleTitle: String? = null,
+    val articleByline: String? = null,
+    val articleLang: String,
+    val articleDir: String? = null,
+    val articleSiteName: String? = null,
+    val attempts: MutableList<Attempt> = mutableListOf(),
+    val metadata: Map<String, String> = mutableMapOf(),
+    val debug: Boolean,
+    val maxElemsToParse: String? = null,
+    val nbTopCandidates: Int,
+    val charThreshold: Int,
+    val classesToPreserve: String? = null,
+    val keepClasses: String? = null,
+    val serializer: String? = null,
+    val disableJSONLD: String? = null,
+    val allowedVideoRegex: Regex,
+    val linkDensityModifier: BigDecimal,
+    val flags: Int,
+) {
+    data class Attempt(
+        val articleContent: Element,
+        val textLength: Int,
+    )
+}
