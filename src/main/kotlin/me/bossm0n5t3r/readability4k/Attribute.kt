a9e1af1c8800ae81872cd0a416c16f0eca1c80bc
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-10-05 19:20:00 +0900

    feat: add `Attribute` class for element attributes
    - Define `Attribute` with name and value properties
    - Implement `getEncodedValue` method for HTML encoding of attribute values
    - Add `cloneNode` method for creating attribute copies

---
+++ src/main/kotlin/me/bossm0n5t3r/readability4k/Attribute.kt
@@ -0,0 +1,10 @@
+package me.bossm0n5t3r.readability4k
+
+data class Attribute(
+    val name: String,
+    var value: String,
+) {
+    fun getEncodedValue(): String = HtmlEntities.encodeHTML(value)
+
+    fun cloneNode(): Attribute = copy()
+}
