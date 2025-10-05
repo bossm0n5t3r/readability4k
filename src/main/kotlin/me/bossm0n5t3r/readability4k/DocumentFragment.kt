f4e87ff837ace12bf186655cda8f962a818de7cd
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-10-05 19:25:00 +0900

    feat: add `DocumentFragment` class for document fragments
    - Define `DocumentFragment` extending `Node`
    - Set `nodeType` to `DOCUMENT_FRAGMENT_NODE` and `nodeName` to `#document-fragment`
    - Add `children` property to manage child elements

---
+++ src/main/kotlin/me/bossm0n5t3r/readability4k/DocumentFragment.kt
@@ -0,0 +1,8 @@
+package me.bossm0n5t3r.readability4k
+
+class DocumentFragment : Node() {
+    override val nodeType = NodeType.DOCUMENT_FRAGMENT_NODE
+    override val nodeName = "#document-fragment"
+
+    val children: MutableList<Element> = mutableListOf()
+}
