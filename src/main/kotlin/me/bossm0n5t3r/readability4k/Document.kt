a35255b6da76236032a7e41c126e270ae7d7294b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-10-05 19:45:00 +0900

    feat: add `Document` class for representing documents
    - Define `Document` extending `Node`
    - Add properties for `documentURI`, `title`, `head`, `body`, and `children`
    - Implement methods for element retrieval, creation, and base URI resolution

---
+++ src/main/kotlin/me/bossm0n5t3r/readability4k/Document.kt
@@ -0,0 +1,65 @@
+package me.bossm0n5t3r.readability4k
+
+import me.bossm0n5t3r.readability4k.NodeUtils.getElementsByTagName
+import java.net.URI
+
+class Document(
+    val documentURI: String,
+) : Node() {
+    override val nodeType = NodeType.DOCUMENT_NODE
+    override val nodeName = "#document"
+
+    var title: String = ""
+    var documentElement: Element? = null
+    var head: Element? = null
+    var body: Element? = null
+
+    val children: MutableList<Element> = mutableListOf()
+
+    private var _baseURI: String? = null
+    val baseURI: String
+        get() {
+            _baseURI?.let { return it }
+
+            var result = documentURI
+
+            getElementsByTagName("base")
+                .firstOrNull()
+                ?.getAttribute("href")
+                ?.let { href ->
+                    try {
+                        result = URI(result).resolve(href).toString()
+                    } catch (e: Exception) {
+                        // Just fall back to documentURI
+                    }
+                }
+
+            _baseURI = result
+            return result
+        }
+
+    fun getElementsByTagName(tag: String): List<Element> = getElementsByTagName(this, tag)
+
+    fun getElementById(id: String): Element? {
+        fun getElem(node: Node): Element? {
+            if (node is Element && node.id == id) {
+                return node
+            }
+            for (child in node.childNodes) {
+                val result = getElem(child)
+                if (result != null) return result
+            }
+            return null
+        }
+        return getElem(this)
+    }
+
+    fun createElement(tag: String): Element = Element(tag)
+
+    fun createTextNode(text: String): TextNode =
+        TextNode().apply {
+            textContent = text
+        }
+
+    fun createDocumentFragment(): DocumentFragment = DocumentFragment()
+}
