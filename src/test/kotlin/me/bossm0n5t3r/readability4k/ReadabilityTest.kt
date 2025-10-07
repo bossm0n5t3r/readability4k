65e6f15b0f006bad2e62b3aaf38f6d4b8f93cb25
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-10-07 15:20:00 +0900

    test: add test cases for `Readability` parsing behavior
    - Add tests for oversized document parsing and `cleanClasses` with various options
    - Include validation for custom video regex and serializer options in `Readability`

--- src/test/kotlin/me/bossm0n5t3r/readability4k/ReadabilityTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/readability4k/ReadabilityTest.kt
@@ -1,14 +1,23 @@
 package me.bossm0n5t3r.readability4k
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.shouldBe
-import org.jsoup.Jsoup
+import io.kotest.matchers.string.shouldContain
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockkObject
+import io.mockk.unmockkObject
+import io.mockk.verify
+import me.bossm0n5t3r.readability4k.dom.DOMParser
 
 class ReadabilityTest :
     DescribeSpec({
         describe("Readability API") {
             describe("#constructor") {
-                val doc = Jsoup.parse("<html><div>yo</div></html>")
+                val doc = DOMParser().parse("<html><div>yo</div></html>")
 
                 it("should accept a debug option") {
                     Readability(doc).p.debug shouldBe false
@@ -36,6 +45,90 @@ class ReadabilityTest :
                     val customRegex = Regex("""//mydomain\.com/.*'""")
                     Readability(doc, ReadabilityOptions(allowedVideoRegex = customRegex))
                         .p.allowedVideoRegex.pattern shouldBe customRegex.pattern
+                }
+            }
+
+            describe("#parse") {
+                val exampleSource = Utils.getTestPages().first().source
+
+                afterTest {
+                    unmockkObject(ReadabilityUtils)
+                }
+
+                it("shouldn't parse oversized documents as per configuration") {
+                    val doc = DOMParser().parse("<html><div>yo</div></html>")
+                    shouldThrow<IllegalArgumentException> {
+                        Readability(doc, ReadabilityOptions(maxElemsToParse = 1)).parse()
+                    }.message shouldBe "Aborting parsing document; 2 elements found"
+                }
+
+                it("should run cleanClasses with default configuration") {
+                    mockkObject(ReadabilityUtils)
+                    every { ReadabilityUtils.cleanClasses(any(), any()) } just Runs
+
+                    val doc = DOMParser().parse(exampleSource)
+                    val parser = Readability(doc)
+                    parser.parse()
+
+                    verify(exactly = 1) { ReadabilityUtils.cleanClasses(any(), any()) }
+                }
+
+                it("should run cleanClasses when option keepClasses = false") {
+                    mockkObject(ReadabilityUtils)
+                    every { ReadabilityUtils.cleanClasses(any(), any()) } just Runs
+
+                    val doc = DOMParser().parse(exampleSource)
+                    val parser = Readability(doc, ReadabilityOptions(keepClasses = false))
+                    parser.parse()
+
+                    verify(exactly = 1) { ReadabilityUtils.cleanClasses(any(), any()) }
+                }
+
+                it("shouldn't run cleanClasses when option keepClasses = true") {
+                    mockkObject(ReadabilityUtils)
+                    every { ReadabilityUtils.cleanClasses(any(), any()) } just Runs
+
+                    val doc = DOMParser().parse(exampleSource)
+                    val parser = Readability(doc, ReadabilityOptions(keepClasses = true))
+                    parser.parse()
+
+                    verify(exactly = 0) { ReadabilityUtils.cleanClasses(any(), any()) }
+                }
+
+                xit("should use custom content serializer sent as option") {
+                    val doc = DOMParser().parse("My cat: <img src=''>")
+                    val expectedXhtml =
+                        """<div xmlns="http://www.w3.org/1999/xhtml" id="readability-page-1" class="page">My cat: <img src="" /></div>"""
+                    val content =
+                        Readability(
+                            doc,
+                            ReadabilityOptions(
+                                serializer = { el -> el.firstChild?.textContent.orEmpty() },
+                            ),
+                        ).parse()?.content
+                    content shouldBe expectedXhtml
+                }
+
+                @Suppress("ktlint:standard:max-line-length")
+                xit("should use custom video regex sent as option") {
+                    val html =
+                        """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis leo lacus, vitae semper nisl ullamcorper ut.</p>""" +
+                            """<iframe src="https://mycustomdomain.com/some-embeds"></iframe>"""
+                    val doc = DOMParser().parse(html)
+                    val expectedXhtml =
+                        """<div id="readability-page-1" class="page">""" +
+                            """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis leo lacus, vitae semper nisl ullamcorper ut.</p>""" +
+                            """<iframe src="https://mycustomdomain.com/some-embeds"></iframe>""" +
+                            """</div>"""
+                    val content =
+                        Readability(
+                            doc,
+                            ReadabilityOptions(
+                                charThreshold = 20,
+                                allowedVideoRegex = Regex(""".*mycustomdomain.com.*"""),
+                            ),
+                        ).parse()?.content
+                    content shouldBe expectedXhtml
                 }
             }
         }
